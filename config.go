59960e005c48d54885f0ec06b0815326511f4678
wgliang <liangcszzu@163.com>
2016-11-26 20:08:20 +0800

    rename monitor

--- config.go
+++ config.go
@@ -7,6 +7,48 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/wgliang/metrics"
+)
+
+// All can collect Type-of-Monitoring-Data. No matter what you add into
+// goappmonitor,all data will one of the types. So you should choose which type
+// is your best choice.
+var (
+	// float64-gauge
+	appGaugeFloat64 = metrics.NewCollectry()
+	// counter
+	appCounter = metrics.NewCollectry()
+	// meter
+	appMeter = metrics.NewCollectry()
+	// histogram
+	appHistogram = metrics.NewCollectry()
+	// debug status data
+	appDebug = metrics.NewCollectry()
+	// runtime statusc data
+	appRuntime = metrics.NewCollectry()
+	// self
+	appSelf = metrics.NewCollectry()
+	// all collect data
+	values = make(map[string]metrics.Collectry)
+
+	// default config
+	configFn     = "./cfg.json" // 配置文件路径
+	defaultTags  = ""           // 标签
+	defaultStep  = int64(60)    // 默认采集频率60s一次
+	defaultBases = []string{}
+	defaultPush  = &PushConfig{Enabled: true, Api: "http://127.0.0.1:1988/v1/push"}
+	defaultHttp  = &HttpConfig{Enabled: false, Listen: ""}
+
+	// global variables
+	cfg      *GlobalConfig
+	cfgLock  = new(sync.RWMutex)
+	step     int64
+	api      string
+	gdebug   bool
+	endpoint string
+	gtags    string
 )
 
 // Global config of goappmonitor, and you can config it in cfg.json.
@@ -32,24 +74,16 @@ type PushConfig struct {
 	Api     string `json:"api"`
 }
 
-var (
-	// default config
-	configFn     = "./cfg.json" // 配置文件路径
-	defaultTags  = ""           // 标签
-	defaultStep  = int64(60)    // 默认采集频率60s一次
-	defaultBases = []string{}
-	defaultPush  = &PushConfig{Enabled: true, Api: "http://127.0.0.1:1988/v1/push"}
-	defaultHttp  = &HttpConfig{Enabled: false, Listen: ""}
-
-	// global variables
-	cfg      *GlobalConfig
-	cfgLock  = new(sync.RWMutex)
-	step     int64
-	api      string
-	gdebug   bool
-	endpoint string
-	gtags    string
-)
+// Initialize all your type.
+func init() {
+	values["gauge"] = appGaugeFloat64
+	values["counter"] = appCounter
+	values["meter"] = appMeter
+	values["histogram"] = appHistogram
+	values["debug"] = appDebug
+	values["runtime"] = appRuntime
+	values["self"] = appSelf
+}
 
 // Get config.
 func config() *GlobalConfig {
@@ -172,4 +206,61 @@ func readFileString(fn string) (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(string(b)), nil
+}
+
+// Return raw data of a metric.
+func rawMetric(types []string) map[string]interface{} {
+	data := make(map[string]interface{})
+	for _, mtype := range types {
+		if v, ok := values[mtype]; ok {
+			data[mtype] = v.Values()
+		}
+	}
+	return data
+}
+
+// Return all-type metrics raw data.
+func rawMetrics() map[string]interface{} {
+	data := make(map[string]interface{})
+	for key, v := range values {
+		data[key] = v.Values()
+	}
+	return data
+}
+
+// Retuen all-type metrics data size.
+func rawSizes() map[string]int64 {
+	data := map[string]int64{}
+	all := int64(0)
+	for key, v := range values {
+		kv := v.Size()
+		all += kv
+		data[key] = kv
+	}
+	data["all"] = all
+	return data
+}
+
+// Collect all base or system data. And it contains debug and runtime status.
+func collectBase(bases []string) {
+	// collect data after 30s
+	time.Sleep(time.Duration(30) * time.Second)
+	// if open debug
+	if contains(bases, "debug") {
+		CollectDebugGCStats(appDebug, 5e9)
+	}
+	// if open runtime
+	if contains(bases, "runtime") {
+		CollectRuntimeMemStats(appRuntime, 5e9)
+	}
+}
+
+// Check base status.
+func contains(bases []string, name string) bool {
+	for _, n := range bases {
+		if n == name {
+			return true
+		}
+	}
+	return false
 }
